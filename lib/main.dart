d246919be32895b79a4e798d7747514330b4279a
David <jdsanchez2023@incos-santacruz.edu.bo>
2025-07-28 21:23:50 -0400

    campos demformulario definidos

--- lib/main.dart
+++ lib/main.dart
@@ -50,6 +50,130 @@ class Formulario extends StatelessWidget {
           children: [
             //crear un container
 
+            Container(
+              padding: EdgeInsets.all(24),//pading interno
+              decoration:  BoxDecoration(//agrega una decoracion
+                color: Colors.white,
+              ),
+
+              child: Column(//declaramos una columna que sera el hijo de forma vertical
+                children: [
+                  Icon(
+                    Icons.person_add,//define un icono de persona
+                    size: 48,
+                    color: primaryNegro,
+                  ),
+
+                  SizedBox(height: 12),
+                    //Definir Titulo despues del icono
+                  Text(
+                    'Datos Personales',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: primaryNegro,
+                    ),
+                  ),
+
+                  //definir el espacio
+                  SizedBox(height: 12),
+
+        //definir otro texto debajo del texto de arriba
+                  Text(
+                    'Complete todos los campos requeridos',
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: primaryVioleta,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            //definir el epsacio enytres contenedores
+
+            SizedBox(height: 24),
+
+
+            //creamos otro contenedor
+
+            Container(
+              padding: EdgeInsets.all(24),
+              decoration: BoxDecoration(
+
+              ),
+
+              child: Column(//declarar un hijo de forma verticla
+                crossAxisAlignment: CrossAxisAlignment.stretch,//estira los widgets para que ocupen el ancho disponible
+                children: [
+                  _buildSeccionTitulo('Informacion', Icons.info_outline),
+
+                  _buildTextField(
+                    label: 'Nombre',
+                    hint: 'ingrese su nombre',
+                    icon: Icons.person,
+                  ),
+
+                  _buildTextField(
+                    label: 'Apellido',
+                    hint: 'ingrese su apellido',
+                    icon: Icons.person,
+                  ),
+                  _buildTextField(
+                    label: 'Correo Electronico',
+                    hint: 'ejemplo@gmail.com',
+                    icon: Icons.email,
+                  ),
+
+                  _buildTextField(
+                    label: 'Direccion',
+                    hint: 'calle,ciudad,pais',
+                    icon: Icons.location_on,
+                  ),
+
+                  _buildTextField(
+                    label: 'Telefono',
+                    hint: '+591 ',
+                    icon: Icons.phone,
+                  ),
+
+                  SizedBox(height: 32,),
+
+                  Row(
+                    children: [
+                      Expanded(
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: primaryNavy,//define el color de fondo
+                            foregroundColor: Colors.white,
+                            padding: EdgeInsets.symmetric(vertical: 16),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            elevation: 2,
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.save, size: 20),
+                              SizedBox(width: 8),
+                              Text(
+                                'Registrar',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+
           ],
         ),
       ),
@@ -57,4 +181,91 @@ class Formulario extends StatelessWidget {
   }
 }
 
+
+//declarar la funcion para el titilo pra el builSeccionTitulo
+Widget _buildSeccionTitulo(String title, IconData icon){
+  return Padding(
+    padding: EdgeInsets.only(bottom: 16),//aplica un espacio inferior de 16 para separara el titulo
+    child: Row(
+      children: [
+        Icon(
+            icon,
+          color: Formulario.primaryVioleta,
+          size: 20,
+        ),
+        //definir el espacio de contenedor a contenedor4
+        SizedBox(width: 8,),
+        //dtitulo de la seccion
+        Text(
+          title,
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,//que el texto sea negrita
+            color: Formulario.primaryVioleta,
+          ),
+        ),
+        Expanded(//toma todo el espacio del widget
+            child: Container(
+              margin: EdgeInsets.only(left: 12),//establece un margen isquierdo de 12 megapuxeles
+              height: 1,
+              color: Formulario.primaryNaranja,
+            ),
+        ),
+      ],
+    ),
+  );
+}
+
+
+//declarar la funcion que se usara para los testos del formulario
+Widget _buildTextField({
+  required String label, //psa un parametro que se usara como titulo o etiqueta
+  required String hint,//se usa como texto de sugerencia dentro del campo que desaparesera al escribir
+  required IconData icon,//agrega un icono al casmpo de texto
+  int maxLines = 1,
+}) {
+  return Padding(
+    // Espaciado inferior entre este campo y el siguiente
+    padding: EdgeInsets.only(bottom: 16),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start, // Alinea a la izquierda
+      children: [
+        // Etiqueta del campo (título encima del campo de entrada)
+        Text(
+          label,
+          style: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.w600,
+            color: Formulario.primaryNegro, // se defini color para los titulos
+          ),
+        ),
+        SizedBox(height: 8), // Espacio entre etiqueta y el campo
+
+
+        Container(
+          decoration: BoxDecoration(
+            color: Formulario.primaryCrema, // se  crea un color para el fondo dentro del campo
+            borderRadius: BorderRadius.circular(8), // Bordes
+            border: Border.all(color: Formulario.primaryNaranja),   // se crea un borde alrededor del campo
+          ),
+
+          // Campo de texto
+          child: TextField(
+            maxLines: maxLines, // Define cuántas líneas acepta el campo
+            decoration: InputDecoration(
+              hintText: hint, // Texto de sugerencia dentro del campo
+              hintStyle: TextStyle(color: Formulario.primarylila),
+              prefixIcon: Icon(icon, color: Formulario.primaryVioleta), // se crea un icono y se pone un color
+              border: InputBorder.none, // Elimina borde por defecto del TextField
+              contentPadding: EdgeInsets.symmetric(
+                horizontal: 16,
+                vertical: 12,
+              ),
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
+}
 
